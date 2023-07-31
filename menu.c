13387f0ffc74b720d06e9a95deeaa33210a25645
BIENVENUE CHEZ CHRIS_POINTEUR <140442396+CHRIS-POINTEUR243@users.noreply.github.com>
2023-07-30 22:53:12 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,10 +1,11 @@
 #include <raylib.h>
 
-typedef struct {
-    bool display;
-    bool startGame;
-    bool openScoreboard;
-    bool exitGame;
+// struct  ndo nosso "menu" recebe 
+typedef struct {        
+    bool display;        // display deve ser exibir
+    bool startGame;      // startGame para um novo jogo
+    bool openScoreboard; // scoreboard para a placar 
+    bool exitGame;       // exitGame para sair
 } Menu;
 
 void DisplayMenu(Menu *menu, int screenWidth, int screenHeight);
@@ -51,7 +52,7 @@ int main() {
     return 0;
 }
 
-void DisplayMenu(Menu *menu, int screenWidth, int screenHeight) {
+void DisplayMenu(Menu *menu, int screenWidth, int screenHeight) { // funcão DispayMenu
     if (menu->display) {
         int initialHeight = 400;
         const int width = 100;
@@ -65,24 +66,26 @@ void DisplayMenu(Menu *menu, int screenWidth, int screenHeight) {
 
         ClearBackground(BLACK);
         DrawTextureEx(linkSprite, spritePosition, spriteRotation, spriteScale, WHITE);
-        DrawText("ZINF", width, initialHeight - 350, 160, WHITE);
-        DrawText("Press ENTER to start", width, initialHeight, 30, WHITE);
-        DrawText("Press S to enter the Scoreboard", width, initialHeight + 75, 30, WHITE);
-        DrawText("Press X to exit", width, initialHeight + 2 * 75, 30, WHITE);
+        // Ajustei a posição e o tamanho da fonte para a palavra *ZINF*****
+        // usei a função MeasureText 
+        DrawText("ZINF", screenWidth / 2 - MeasureText("ZINF", 160) / 2, initialHeight - 250, 160, YELLOW); // deixei amarelo**
+        DrawText("Press ENTER to start", width, initialHeight, 30, YELLOW);
+        DrawText("Press S to enter the Scoreboard", width, initialHeight + 75, 30, YELLOW);
+        DrawText("Press X to exit", width, initialHeight + 2 * 75, 30, YELLOW);
 
         EndDrawing();
 
-        if (IsKeyPressed(KEY_ENTER)) {
+        if (IsKeyPressed(KEY_ENTER)) {  // pressionar "enter" para novo jogo
             menu->display = false;
             menu->startGame = true;
         }
 
-        if (IsKeyPressed(KEY_S)) {
+        if (IsKeyPressed(KEY_S)) {        // pressionar "S" para placar 
             menu->display = false;
             menu->openScoreboard = true;
         }
 
-        if (IsKeyPressed(KEY_X)) {
+        if (IsKeyPressed(KEY_X)) {        // pressionar "X' para sair do menu
             menu->display = false;
             menu->exitGame = true;
         }
